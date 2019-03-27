2d0fc0da8dc6a1382b57186474f003492e35ca7b
Errol <s20a3264@hotmail.com>
2019-03-28 00:17:46 +0800

    add fontawesome

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './css/application.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
